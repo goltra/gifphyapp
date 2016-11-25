b309b3a55db269cde6970d03caa830f3ad0d680c
Francisco Garcia <franciscogarcia@iMac-de-Francisco.local>
2016-11-25 09:54:37 +0100

    ficheos que faltaban

---
+++ src/providers/giphy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class GiphyService {
+  rootUrl: string;
+  path: string;
+  key:string;
+
+  constructor(public http: Http) {
+    console.log('Giphy Provider');
+    this.key = "dc6zaTOxFJmzC";
+    this.rootUrl = "http://api.giphy.com";
+    this.path = "/v1/gifs/"
+  }
+
+  getTrending(){
+    let url = this.rootUrl+this.path+"trending?fmt=json&api_key=" + this.key;
+    return this.http.get(url)
+      .map(res=> res.json());
+
+  }
+
+}
